25c9888aa51ad66358bdd8613763f0d0794d0d4e
Antonio <tofernandes67@gmail.com>
2021-02-22 20:27:07 +0000

    change application cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   acts_as_token_authentication_handler_for User, except: [:create], fallback: :exception
   def cors_preflight_check
     return unless request.method == 'OPTIONS'
+
     cors_set_access_control_headers
     render json: {}
   end
@@ -13,8 +14,8 @@ class ApplicationController < ActionController::Base
   def cors_set_access_control_headers
     response.headers['Cache-Control'] = 'max-age=3'
     #  response.headers['Access-Control-Allow-Origin'] = 'https://terrincha.herokuapp.com'
-    # response.headers['Access-Control-Allow-Origin'] = 'https://designdosorriso.netlify.app'
-    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    response.headers['Access-Control-Allow-Origin'] = 'https://designdosorriso.netlify.app'
+    # response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
     response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, ' \
       'Auth-Token, Email, X-User-Token, X-User-Email, x-xsrf-token'
