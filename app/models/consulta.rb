6ea79fb33c78da2f5faaa9110da451ebe01b69bd
Antonio <tofernandes67@gmail.com>
2021-02-28 20:52:50 +0000

    add bocas to destroy

--- app/models/consulta.rb
+++ app/models/consulta.rb
@@ -3,5 +3,5 @@ class Consulta < ApplicationRecord
   has_one_attached :image
   has_many :consulta_details, dependent: :destroy
   has_many :pagamentos, dependent: :destroy
-
+  has_many :bocas, dependent: :destroy
 end
